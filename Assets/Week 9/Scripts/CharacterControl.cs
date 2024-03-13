c728652beba393f10ad07e559155512c0a2cfadf
munozsev <munozsev@OA-S141-02>
2024-03-13 12:05:48 -0400

    Task 4b: UI displays selected character type

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    public TextMeshProUGUI typeText;
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
@@ -15,5 +18,15 @@ public class CharacterControl : MonoBehaviour
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
     }
-    
+
+    private void Update()
+    {
+        if (SelectedVillager != null)
+        {
+            typeText.text = SelectedVillager.GetType().ToString();
+        } else
+        {
+            typeText.text = "No Selection";
+        }
+    }
 }
