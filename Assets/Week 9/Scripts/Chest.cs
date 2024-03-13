1cc7d21fead81c784c2a50d2deca4cbfabf5b62a
munozsev <munozsev@OA-S141-02>
2024-03-13 10:55:11 -0400

    Task 2 completed: chests open for respective types

--- Assets/Week 9/Scripts/Chest.cs
+++ Assets/Week 9/Scripts/Chest.cs
@@ -2,13 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum ChestType { Villager, Merchant, Archer}
 public class Chest : MonoBehaviour
 {
     public Animator animator;
+    public ChestType whoCanOpen;
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        animator.SetBool("IsOpened", true);
+        if(collision.TryGetComponent<Villager>(out Villager villager))
+        {
+            Debug.Log("check 1");
+            if (villager.CanOpen() == whoCanOpen || whoCanOpen == ChestType.Villager)
+            {
+                Debug.Log("check 2");
+                animator.SetBool("IsOpened", true);
+            }
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
