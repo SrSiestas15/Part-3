9801367612d2de5dacfce148881e22d6165e112d
munozsev <munozsev@OA-S141-02>
2024-03-13 10:27:40 -0400

    Task 1a completed: Archer and Merchant working

---
+++ Assets/Week 9/Scripts/Archer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : Villager
+{
+    public GameObject arrowPrefab;
+    public Transform arrowSpawn;
+
+    protected override void Attack()
+    {
+        destination = transform.position;
+        base.Attack();
+        Instantiate(arrowPrefab, arrowSpawn.position, arrowSpawn.rotation);
+    }
+}
